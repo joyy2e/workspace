19addbf1efa59198f59bb0afb449a52160d45f7b
joyfulwave <luvjoyyy@hanmail.net>
2022-09-21 17:36:15 +0900

    추가

---
+++ project/03.js/welcome.js
@@ -0,0 +1,12 @@
+document.writeln('자바스크립트 강의입니다.');
+document.writeln('안녕하세요? 반갑습니다.')
+
+console.log("자바스크립트 강의입니다.");
+console.log("콘솔창에 글씨가 나오다니");
+console.log("정말 신기한 일 아니겠습니까?");
+
+console.dir("출력할 내용");
+
+console.error("에러입니다.");
+
+alert("출력할 내용");
